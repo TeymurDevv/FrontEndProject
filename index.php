33b9a86fdf845bbe9ae04364c1b25f2fe65e4d97
TeymurDevv <transmissiyaservis@gmail.com>
2024-04-18 07:00:37 -0700

    Category Slider Without buttons is done Button CSS will be added soon

--- index.php
+++ index.php
@@ -1111,5 +1111,26 @@
         });
     });
 
+    $('.category-slider').slick({
+
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        autoplay: true, responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+        ],
+        prevArrow: ' <span class="previous_arrow"><i class="fa-solid fa-arrow-left"></i></span>',
+        nextArrow: ' <span class="next_arrow"><i class="fa-solid fa-arrow-right"></i></span>'
+    });
+
 
 </script>
