a4e116e2442c8448d8be67d337199d575719a83e
TeymurDevv <transmissiyaservis@gmail.com>
2024-04-18 09:55:39 -0700

    HTML layout ready for New Arrivals

--- index.php
+++ index.php
@@ -1467,6 +1467,75 @@
         </div>
     </section>
 
+    <section id="new-arrivals">
+        <div class="container">
+            <div class="row mb-5 row-style">
+                <div class="col-lg-12">
+                    <div class="d-flex gap-3 mt-5 mb-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="red"
+                            class="injected-svg" data-src="/assets/images/icons/new-product-1.svg"
+                            xmlns:xlink="http://www.w3.org/1999/xlink" role="img">
+                            <g clip-path="url(#clip0-76)">
+                                <path
+                                    d="M22.9347 12.001L24 10.1003L22.3995 8.62199L22.8254 6.48519L20.8463 5.57376L20.5911 3.40991L18.4273 3.15468L17.5158 1.17562L15.379 1.60157L13.9006 0.000976562L12 1.0663L10.0994 0.000976562L8.62106 1.60152L6.48427 1.17557L5.57283 3.15463L3.40898 3.40987L3.15375 5.57371L1.17469 6.48515L1.60064 8.62194L0 10.1003L1.06533 12.0009L0 13.9016L1.60055 15.3799L1.17459 17.5167L3.15366 18.4281L3.40889 20.592L5.57273 20.8472L6.48417 22.8263L8.62097 22.4003L10.0993 24.0009L11.9999 22.9356L13.9005 24.0009L15.3788 22.4003L17.5156 22.8263L18.4271 20.8472L20.5909 20.592L20.8462 18.4281L22.8252 17.5167L22.3993 15.3799L23.9998 13.9016L22.9347 12.001ZM8.62936 14.0641H7.61878L5.78016 11.7887V14.0641H4.50881V9.43504H5.49984L7.35802 11.7887V9.43504H8.62936V14.0641ZM12.7238 14.0641H9.42483V9.43504H12.6652V10.5434H10.6962V11.1954H12.3783V12.2255H10.6962V12.9557H12.7239V14.0641H12.7238ZM18.0765 14.0641H17.0203L16.3618 12.408L15.7098 14.0641H14.6536L12.8411 9.43504H14.2168L15.2209 12.6102L15.5403 11.6453L14.7905 9.43504H15.9576L16.3618 10.8629L16.7726 9.43504H17.9397L17.1768 11.6453L17.5094 12.6102L18.507 9.43504H19.8891L18.0765 14.0641Z"
+                                    fill="red"></path>
+                            </g>
+                            <defs>
+                                <clipPath id="clip0-76">
+                                    <rect width="24" height="24" fill="white"></rect>
+                                </clipPath>
+                            </defs>
+                        </svg>
+                        <h2 class="sc-a7fb2b58-0 EKIZr">New Arrivals</h2>
+                    </div>
+                </div>
+                <div class="col-lg-12 d-flex">
+                    <div class="row d-flex justify-content-center">
+                        <div class="new-arrivals-product d-flex flex-wrap align-items-stretch justify-content-center">
+                            <!-- Keep as row -->
+                            <div class="col-md-4 col-lg-2 arrivals-product p1 text-center">
+                                <!-- Keep as row -->
+                                <img src="./assets/img/imagegoggles.webp" alt="" style="max-width: 100%; height: auto;">
+                                <h4 class="camera">Sunglass</h4>
+                                <h4 class="priceSmall">$150.00</h4>
+                            </div>
+                            <div class="col-md-4 col-lg-2 arrivals-product text-center">
+                                <!-- Keep as row -->
+                                <img src="./assets/img/lipstick (2).webp" alt="" style="max-width: 100%; height: auto;">
+                                <h4 class="camera">Makeup</h4>
+                                <h4 class="priceSmall">$250.00</h4>
+                            </div>
+                            <!-- Repeat the same structure for other items -->
+                            <div class="col-md-4 col-lg-2 arrivals-product text-center">
+                                <img src="./assets/img/bgwatch (1).webp"
+                                    style="max-width: 100%; height: auto; border-radius: 12px;" alt="">
+                                <h4 class="camera">Smart Watch</h4>
+                                <h4 class="priceSmall">$350.00</h4>
+                            </div>
+                            <div class="col-md-4 col-lg-2 arrivals-product text-center">
+                                <img src="./assets/img/lipstick (1).webp" alt="" style="max-width: 100%; height: auto;">
+                                <h4 class="camera">Lipstick</h4>
+                                <h4 class="priceSmall">$15.00</h4>
+                            </div>
+                            <div class="col-md-4 col-lg-2 arrivals-product text-center">
+                                <img src="./assets/img/lipstick (4).webp" alt="" style="max-width: 100%; height: auto;">
+                                <h4 class="camera">Green plant</h4>
+                                <h4 class="priceSmall">$55.00</h4>
+                            </div>
+                            <div class="col-md-4 col-lg-2 arrivals-product text-center">
+                                <!-- Keep as row -->
+                                <img src="./assets/img/lipstick (3).webp" alt="" style="max-width: 100%; height: auto;">
+                                <h4 class="camera">Bonsai tree</h4>
+                                <h4 class="priceSmall">$535.00</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
 
 
 
