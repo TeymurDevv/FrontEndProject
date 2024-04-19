165f8060a9e53dcab0a86ae56c4ec10535749d3f
TeymurDevv <transmissiyaservis@gmail.com>
2024-04-19 09:21:39 -0700

    Product Page Created without Libraries Implementation

--- product.php
+++ product.php
@@ -1,1 +1,21 @@
-<?php require("./layouts/header.php") ?>;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bonik | Single Product</title>
+</head>
+<body>
+    <link rel="stylesheet" href="./assets/css/header.css">
+    <link rel="stylesheet" href="./assets/css/header2.css">
+    <link rel="stylesheet" href="./assets/css/sidebar.css">
+    <link rel="stylesheet" href="./assets/css/footer.css">
+
+    <?php 
+    require("./layouts/header.php");
+    require("./layouts/header1.php");
+    require("./layouts/sidebar.php");
+    require("./layouts/footer.php");
+    ?>
+</body>
+</html>
