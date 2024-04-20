593be1a74525652fe51e65ef010f73f01e11c436
TeymurDevv <transmissiyaservis@gmail.com>
2024-04-20 08:26:13 -0700

    Partially Passed Stage Design is Complete

--- checkout.php
+++ checkout.php
@@ -3,6 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/bootstrap-5.3.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/header.css">
+    <link rel="stylesheet" href="./assets/css/header2.css">
+    <link rel="stylesheet" href="./assets/css/sidebar.css">
+    <link rel="stylesheet" href="./assets/css/slider.css">
+    <link rel="stylesheet" href="./assets/css/flash-products.css">
     <title>Document</title>
 </head>
 <body>
@@ -19,19 +25,19 @@
             <div class="col-lg-8">
 
                 <div class="stage">
-                    <div>
+                    <div class="stage-passed">
                         1. Cart
                     </div>
 
-                    <div>
+                    <div class="passed-line">
 
                     </div>
 
-                    <div>
+                    <div class="stage-passed">
                         2. Details
                     </div>
 
-                    <div>
+                    <div class="passed-line">
 
                     </div>
 
@@ -58,6 +64,11 @@
 
     <?php require("./layouts/footer.php"); ?>
 
+    <style>
+        .CategoriesPart {
+            display: none;
+        }
+    </style> 
     
 </body>
 </html>
