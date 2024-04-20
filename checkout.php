72f7518cf367cc8f56306c24f9a1973fa16511af
TeymurDevv <transmissiyaservis@gmail.com>
2024-04-20 08:15:35 -0700

    Stage layout for Checkout page is ready

--- checkout.php
+++ checkout.php
@@ -7,5 +7,57 @@
 </head>
 <body>
     
+    <?php 
+    require("./layouts/header.php");
+    require("./layouts/header1.php");
+    require("./layouts/sidebar.php");
+    ?>
+
+    <main>
+
+        <div class="container">
+            <div class="col-lg-8">
+
+                <div class="stage">
+                    <div>
+                        1. Cart
+                    </div>
+
+                    <div>
+
+                    </div>
+
+                    <div>
+                        2. Details
+                    </div>
+
+                    <div>
+
+                    </div>
+
+                    <div>
+                        3. Payment
+                    </div>
+
+                    <div>
+
+                    </div>
+
+                    <div>
+                        4. Renew
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-lg-4">
+
+            </div>
+        </div>
+
+    </main>
+
+    <?php require("./layouts/footer.php"); ?>
+
+    
 </body>
 </html>
